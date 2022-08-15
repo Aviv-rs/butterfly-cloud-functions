4410a3c47e9389f4e7a22e478d100ee1ae39e888
yonatan104 <yonatangeller@gmail.com>
2022-08-15 11:32:02 +0300

    c

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
+import * as nodemailer from "nodemailer"
 import * as functions from "firebase-functions";
-import { sendEmail } from "./services/digital.service"
+
+var transporter = nodemailer.createTransport({
+  service: 'hotmail', // outlook's service
+  port: 587,
+  auth: {
+    // TODO: put in env variables
+    user: 'butterfly.test@outlook.com', // mail to send from (gmail does not work!)
+    pass: 'Butterfly123', // mail password
+  },
+})
+
+
+async function sendEmail(email: string) {
+  await transporter.sendMail({
+    from: 'Butterfly <butterfly.test@outlook.com>', // sender address
+    to: email, // list of receivers
+    subject: 'Hello', // Subject line
+    text: 'Hello World', // plain text body
+    html: '<b>Hello World</b>', // html body
+  })
+
+}
 
 exports.newApiKey = functions.firestore
   .document("API_KEYS/{apiKey}")
   .onCreate((snap, context) => {
+    // functions.logger.log('Uppercasing', snap);
     sendEmail("judgefudge315@gmail.com");
   });
