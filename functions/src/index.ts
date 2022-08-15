7a0598de41c0d6ced139a3a273427a4f37f75996
Aviv <aviv80010@gmail.com>
2022-08-15 09:58:48 +0300

    using oncreate cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from "firebase-functions";
 
-functions.firestore.document("API_KEYS/{apiKey}").onCreate((snap, context) => {
-  functions.logger.info(snap, { structuredData: true });
-});
+exports.newApiKey = functions.firestore
+  .document("API_KEYS/{apiKey}")
+  .onCreate((snap, context) => {
+    functions.logger.info(snap, { structuredData: true });
+  });
